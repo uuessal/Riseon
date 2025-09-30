1a1b94e69b4c4da4aaeccfdad2d9e0e328ad8465
wessal <wessal.h.alharbi@outlook.com>
2025-09-30 09:12:47 +0300

    Initial Commit

---
+++ RiseonTests/RiseonTests.swift
@@ -0,0 +1,17 @@
+//
+//  RiseonTests.swift
+//  RiseonTests
+//
+//  Created by wessal hashim alharbi on 03/04/1447 AH.
+//
+
+import Testing
+@testable import Riseon
+
+struct RiseonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

e71c84f8fba511422ba27ce1c446c76836fff389
wessal <wessal.h.alharbi@outlook.com>
2025-09-30 09:12:47 +0300

    Initial Commit

---
+++ Riseon/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Riseon
+//
+//  Created by wessal hashim alharbi on 03/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack
+        {
+            Image("background")
+            
+            VStack {
+                
+                Image("logo").resizable()
+                    .scaledToFit()
+                    .frame(width: 400)
+            }
+                
+        }
+   
+        .padding()
+    }
+}
+
+struct ContentView2: View {
+    var body: some View {
+        
+        ZStack
+        {
+            Image("background")
+            
+            ZStack {
+                
+                Image("Circle").resizable()
+                    .scaledToFit()
+                    .frame(width: 600)
+                
+             
+                
+                Text("Hello, World!")
+                    .foregroundColor(.white).font(.system(size: 20, weight: .bold, design: .default))
+                
+                
+            }
+                
+        }
+   
+        .padding()
+    }
+}
+
+
+
+
+#Preview {
+    
+  ContentView2()
+
+
+    
+   // ContentView()
+}
---
+++ Riseon/RiseonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseonApp.swift
+//  Riseon
+//
+//  Created by wessal hashim alharbi on 03/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct RiseonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
